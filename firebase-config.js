4a23a5b4290605c125153b2ef9165c942b355401
cpachecorep <cpacheco@repcontver.com>
2026-02-23 08:26:34 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyDmN6eko7hlwWwYja9bAn4U34GXjOGkSZY",
+    authDomain: "repcontver-app-time.firebaseapp.com",
+    projectId: "repcontver-app-time",
+    storageBucket: "repcontver-app-time.firebasestorage.app",
+    messagingSenderId: "562206423224",
+    appId: "1:562206423224:web:bfd4b40d3fe10c19d85c4d"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+console.log("🔥 Firebase conectado correctamente");
